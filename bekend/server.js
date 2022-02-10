ea11b841aea30000a1d4753d961602fd8ce8d7c7
Veljko Mijovic Uskovic <veki.uskovic@gmail.com>
2022-02-10 01:48:01 -0800

    dodavanje uputstva

--- bekend/server.js
+++ bekend/server.js
@@ -19,46 +19,6 @@ app.use(cors({
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE"
 }));
 
-app.get("/test1", (req, res)=>{
-    console.log("Radim!");
-    PostModel.create({
-    user: "WekiTest",
-    approvals: 55,
-    disapprovals: 15,
-    comments: [{
-        user: "SaraTest",
-        commentText: "Ovo je sranje post!"
-    },
-    {
-        user: "TineTest",
-        commentText: "nema cao za tebe!"
-    }],
-    text: "Cao svima!",
-    link: "nesto",
-    tag: "svasta"
-    });
-});
-
-app.get("/test2", (req, res)=>{
-    console.log("radim!");
-
-    PostModel.updateMany({user: "WekiTest"}, {
-        $push : { // ovo ne radu nemojte ovako da radite nzm sta mu je 
-            comments :  {
-                     "user": "Misko",
-                     "commentText": "Treab platis struju!"
-                   } //inserted data is the object to be inserted 
-          }
-    });
-});
-
-app.get("/test3", (req, res)=>{
-    const m = PostModel.find({user: "WekiTest"}, (err, data)=>{
-        console.log(data);
-        console.log("..................." + data[0]._id);
-    });
-   // console.log(m);
-});
 
 app.use("/userdata",userRouter);
 app.use("/", postsRouter);
